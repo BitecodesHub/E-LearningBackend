b5e70bdabb244bbcc7ff9e007ef4ff1e547c2a38
BitecodesHub <bitecodes.global@gmail.com>
2025-02-16 17:41:30 +0530

    Added Admin page

--- src/main/java/com/elearning/controller/CourseController.java
+++ src/main/java/com/elearning/controller/CourseController.java
@@ -1,8 +1,11 @@
 package com.elearning.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,6 +38,24 @@ public class CourseController {
 		 catch (Exception e) {
 			 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Course Not Found" );
 		}
+		
+	 }
+	 @DeleteMapping("/deletecourse/{id}")
+	 public ResponseEntity<String> deleteCourseById(@PathVariable Long id) {
+		 try {
+			 courseService.deleteCourseById(id);
+		return ResponseEntity.status(HttpStatus.OK).body("Course Deleted Successfully");
+		 }
+		 catch (Exception e) {
+			 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Course Not Found" );
+		}
+		
+	 }
+	
+	 @GetMapping("/getCourses")
+	 public ResponseEntity<List<Course>> getAllCourses() {
+		 List<Course> allcourses=courseService.getAllCourses();
+		return ResponseEntity.status(HttpStatus.OK).body(allcourses );
 		
 	 }
 	
--- src/main/java/com/elearning/services/CourseService.java
+++ src/main/java/com/elearning/services/CourseService.java
@@ -1,5 +1,7 @@
 package com.elearning.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +20,12 @@ public class CourseService {
 	 public String getCourseName(Long id) {
 		 Course obj1=courseRepository.getById(id);
 		 return obj1.getName();
+		 }
+	 public List<Course> getAllCourses() {
+		 List<Course> allcourses =courseRepository.findAll();
+		 return allcourses;
+		 }
+	 public void deleteCourseById(Long id) {
+		courseRepository.deleteById(id);
 		 }
 }
